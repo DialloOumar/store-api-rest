c3efe6324f7b9a3359d174d5185236b2411da740
Alpha Oumar Diallo <oumardiallo636@gmail.com>
2024-06-04 06:46:54 +0200

    Connected sqlalchamy orm

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
+import os
 import uuid
 from flask import Flask, request, jsonify
-from db import stores, items
+import models
 from flask_smorest import Api
 from resources.store import blp as store_blp
 from resources.item import blp as item_blp
+from db import db 
 
+def create_app(db_url=None):
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config['API_TITLE'] = 'Store REST API'
-app.config['API_VERSION'] = 'v1'
-app.config['OPENAPI_VERSION'] = '3.0.3'
-app.config['OPENAPI_URL_PREFIX'] = '/'
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config['API_TITLE'] = 'Store REST API'
+    app.config['API_VERSION'] = 'v1'
+    app.config['OPENAPI_VERSION'] = '3.0.3'
+    app.config['OPENAPI_URL_PREFIX'] = '/'
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHELY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
+    api = Api(app)
 
-api = Api(app)
+    with app.app_context():
+        db.create_all()
 
-api.register_blueprint(store_blp)
-api.register_blueprint(item_blp)
+    api.register_blueprint(store_blp)
+    api.register_blueprint(item_blp)
+
+    return app
--- models/item.py
+++ models/item.py
@@ -6,7 +6,7 @@ class ItemModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    price = db.Column(db.Float(precision=2), nunique=False, nullable=False)
+    price = db.Column(db.Float(precision=2), unique=False, nullable=False)
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'), nullable=False, unique=False)
     store = db.relationship('StoreModel', back_populates='items')
     
